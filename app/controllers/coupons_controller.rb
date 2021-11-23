646d53de4b2b36e4a5d863a0f6945df9ae8d9fc0
Saika <kyon4501@gmail.com>
2021-11-23 14:57:19 +0800

    fix number

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -15,7 +15,7 @@ class CouponsController < ApplicationController
     if params[:name].blank? || params[:phone].blank? || params[:phone].blank? 
       redirect_to coupon_path, notice: "沒填喔"
     elsif TwidValidator.valid?(params[:twid])
-      redirect_to coupons_path, notice: "已成功兌換，您的序號為#{num} "
+      redirect_to coupons_path, notice: "已成功兌換，您的序號為#{num.join('')}"
     else 
       redirect_to coupon_path, notice: "您的身分證字號有誤，請確認後重新輸入"
   end
